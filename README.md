f4ed3967045b9e8688f25c7c99df5e7bce71f9b4
Simon Leyder <sim.leyder@gmail.com>
2014-12-09 10:45:50 +0100

    Initial commit
    design files / mock up Grunt / .gitignore / README.md

--- README.md
+++ README.md
@@ -7,3 +7,14 @@
 Réalisation d'un design pour un e-commerce lors des périodes de fête, contrainte technique dans la mise en place du **CSS**
 
 Travail sur base du site web [Woo4men](http://woo4men.com/fr/)
+
+
+***
+
+### Compiler le mockup depuis les sources
+
+1. Cloner le repo
+1. Se placer dans le répertoire `webSite`
+1. Lancer `npm install` pour avoir les dépendances npm
+1. Lancer `grunt build` pour compiler le site
+2. Lancer `grunt watch` pour compiler à la sauvegarde des fichiers
---
+++ mockUp/Gruntfile.js
@@ -0,0 +1,63 @@
+"user strict";
+
+module.exports = function( grunt ) {
+
+	grunt.loadNpmTasks( "grunt-contrib-sass" );
+	grunt.loadNpmTasks( "grunt-contrib-jade" );
+	grunt.loadNpmTasks( "grunt-contrib-uglify" );
+	grunt.loadNpmTasks( "grunt-contrib-watch" );
+
+
+	grunt.initConfig( {
+		"sass": {
+			"styles": {
+		      "options": {
+		        "style": 'compressed'
+		      },
+		      "files": {
+		        "bin/css/screen.css": "src/sass/screen.sass"
+		      }
+		    }
+		},
+		"jade": {
+			"jades": {
+				"options": {
+					"pretty": true,
+					"data": {
+						"debug": false,
+					},
+				},
+				"files": {
+					"bin/index.html": "src/jade/index.jade"
+				}
+			}
+		},
+		"uglify": {
+			"scripts": {
+				"files": {
+					"bin/js/script.min.js": "src/js/script.js"
+				}
+			}
+		},
+		"watch": {
+			"styles": {
+				"files": [ "src/sass/*.sass" ],
+				"tasks": [ "sass:styles" ]
+			},
+			"jades": {
+				"files": [ "src/jade/*.jade" ],
+				"tasks": [ "jade:jades" ]
+			},
+			"scripts": {
+				"files": [ "src/js/script.js" ],
+				"tasks": [ "uglify:scripts" ]
+			}
+		}
+	} );
+
+	grunt.registerTask( "build", [
+		"jade",
+		"sass",
+		"uglify"
+	] );
+};

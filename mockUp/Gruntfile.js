46f02d417bcb8960edb7dba1dc7a2f8a1cf41b70
Simon <Sim.Leyder@gmail.com>
2015-01-12 12:53:43 +0100

    Init my own worflow

--- mockUp/Gruntfile.js
+++ mockUp/Gruntfile.js
@@ -2,71 +2,76 @@
 
 module.exports = function( grunt ) {
 
-	grunt.loadNpmTasks( "grunt-contrib-sass" );
+	grunt.loadNpmTasks( "kouto-swiss" );
+	grunt.loadNpmTasks( "grunt-contrib-stylus" );
 	grunt.loadNpmTasks( "grunt-contrib-jade" );
 	grunt.loadNpmTasks( "grunt-contrib-uglify" );
+	grunt.loadNpmTasks( "grunt-notify" );
 	grunt.loadNpmTasks( "grunt-contrib-watch" );
 
 
 	grunt.initConfig( {
-		"sass": {
+		"stylus": {
 			"styles": {
 		      "options": {
-		        "style": 'nested'
+		        "compress": false, // - Change value to have lisible CSS
+				"use": [
+					require("kouto-swiss")
+				]
 		      },
 		      "files": {
-		        "bin/css/screen.css": "src/sass/screen.sass"
+		        "bin/css/main.css": "src/styl/main.styl"
 		      }
 		    }
 		},
 		"jade": {
-			"jades": {
+			"html": {
 				"options": {
-					"pretty": false,
+					"pretty": false, // - Change value to have lisible HTML
 					"data": {
 						"debug": false,
 					},
 				},
-				"files": {
-					"bin/html/index.html": "src/jade/index.jade",
-					"bin/html/category-men.html": "src/jade/category-men.jade",
-					"bin/html/category-men-chemise.html": "src/jade/category-men-chemise.jade",
-					"bin/html/category-men-polo.html": "src/jade/category-men-polo.jade",
-					"bin/html/category-men-pull.html": "src/jade/category-men-pull.jade",
-					"bin/html/category-women.html": "src/jade/category-women.jade",
-					"bin/html/product.html": "src/jade/product.jade",
-					"bin/html/shop.html": "src/jade/shop.jade",
-					"bin/html/contact.html": "src/jade/contact.jade"
+				"files": { // - Add jade's fill to compile here
+					"bin/index.html": "src/jade/main.jade"
 				}
 			}
 		},
 		"uglify": {
 			"scripts": {
 				"files": {
-					"bin/js/script.min.js": "src/js/script.js"
+					"bin/js/script.js": "src/js/script.js"
 				}
+			}
+		},
+		"notify_hooks": {
+			"options": {
+				"enable": true,
+				"success": true, // - Change value to see notify only for errors
+				"duration": 1
 			}
 		},
 		"watch": {
 			"styles": {
-				"files": [ "src/sass/**/*.sass" ],
-				"tasks": [ "sass:styles" ]
+				"files": [ "src/styl/**/*.styl" ],
+				"tasks": [ "stylus:styles", "notify_hooks" ]
 			},
-			"jades": {
+			"html": {
 				"files": [ "src/jade/**/*.jade" ],
-				"tasks": [ "jade:jades" ]
+				"tasks": [ "jade:html", "notify_hooks" ]
 			},
 			"scripts": {
 				"files": [ "src/js/script.js" ],
-				"tasks": [ "uglify:scripts" ]
+				"tasks": [ "uglify:scripts", "notify_hooks" ]
 			}
 		}
 	} );
 
 	grunt.registerTask( "work", [
 		"jade",
-		"sass",
+		"stylus",
 		"uglify",
+		"notify_hooks",
 		"watch"
 	] );
 };
